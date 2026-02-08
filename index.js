fd2f03e1f3263780507dfff868ae99096ce311b8
leandrojofre <leandrotomasjofre@gmail.com>
2026-02-08 06:55:55 -0300

    Fix - Initialize empty number inputs with zero in chat

--- index.js
+++ index.js
@@ -538,7 +538,7 @@ function addTracker(status, character) {
             const input = `
                 <input type="text" value="${value === "" ? "0" : value}" inputmode="decimal" autocomplete="off" data-pattern="^-?\\d+\\.?\\d*$" class="type-number fake-input chat-input-editor" size="0" />
                 <span class="text-quote">
-                    <span class="value font-monospace">${value}</span>
+                    <span class="value font-monospace">${value === "" ? "0" : value}</span>
                     <span class="d-inline-flex gap-0 text-body cursor-pointer fs-normal">
                         <span class="fa-solid fa-caret-left m-0 chat-input-icon select-none opacity-60"></span>
                         <span class="fa-solid fa-caret-right m-0 chat-input-icon select-none"></span>
